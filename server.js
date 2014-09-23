c9d3766769471cf77bff0e858d156aec4263fbcc
timjalexander <timjalexand@gmail.com>
2014-09-23 19:18:42 +0000

    added HTML5 Boilerplate support

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
-var configDB = require('./config/database.js');
+//var configDB = require('./config/database.js');
+var configDB = require('./config/secretDatabase.js');
 
 
 // configuration ===============================================================
