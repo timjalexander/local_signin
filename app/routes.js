942a4eef7a32cf37a04ae777401be481c3c8754a
timjalexander <timjalexand@gmail.com>
2014-09-23 15:51:09 +0000

    fix login issues and cleanup routes

--- app/routes.js
+++ app/routes.js
@@ -51,20 +51,6 @@ module.exports = function(app, passport) {
 			failureFlash : true // allow flash messages
 		}));
 
-	
-// =============================================================================
-// AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
-// =============================================================================
-
-	// locally --------------------------------
-		app.get('/connect/local', function(req, res) {
-			res.render('connect-local.ejs', { message: req.flash('loginMessage') });
-		});
-		app.post('/connect/local', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
 
 	
 
--- config/passport.js
+++ config/passport.js
@@ -75,6 +75,7 @@ module.exports = function(passport) {
     function(req, email, password, done) {
         if (email)
             email = email.toLowerCase(); // Use lower-case e-mails to avoid case-sensitive e-mail matching
+        	console.log("signup email: " + email);
 
         // asynchronous
         process.nextTick(function() {
@@ -82,12 +83,16 @@ module.exports = function(passport) {
             if (!req.user) {
                 User.findOne({ 'local.email' :  email }, function(err, user) {
                     // if there are any errors, return the error
-                    if (err)
+                    if (err){
+                        console.log("error1: " + err);
                         return done(err);
+                    }
 
                     // check to see if theres already a user with that email
                     if (user) {
+                        console.log("user already taken");
                         return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                        
                     } else {
 
                         // create the user
@@ -97,37 +102,16 @@ module.exports = function(passport) {
                         newUser.local.password = newUser.generateHash(password);
 
                         newUser.save(function(err) {
-                            if (err)
+                            if (err) {
+                                console.log("error2: " + err);
                                 return done(err);
-
+                            }
                             return done(null, newUser);
                         });
                     }
 
                 });
             // if the user is logged in but has no local account...
-            } else if ( !req.user.local.email ) {
-                // ...presumably they're trying to connect a local account
-                // BUT let's check if the email used to connect a local account is being used by another user
-                User.findOne({ 'local.email' :  email }, function(err, user) {
-                    if (err)
-                        return done(err);
-                    
-                    if (user) {
-                        return done(null, false, req.flash('loginMessage', 'That email is already taken.'));
-                        // Using 'loginMessage instead of signupMessage because it's used by /connect/local'
-                    } else {
-                        var user = req.user;
-                        user.local.email = email;
-                        user.local.password = user.generateHash(password);
-                        user.save(function (err) {
-                            if (err)
-                                return done(err);
-                            
-                            return done(null,user);
-                        });
-                    }
-                });
             } else {
                 // user is logged in and already has a local account. Ignore signup. (You should log out before trying to create a new account, user!)
                 return done(null, req.user);
